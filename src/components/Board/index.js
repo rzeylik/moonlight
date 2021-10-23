fd91d8828c5f23a7a3264ebeadfedf74a89d8c43
fasul <fasulyak03@gmail.com>
2021-10-23 00:53:48 -0700

    conflict

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -13,7 +13,7 @@ const Board = () => {
                 <p className="nameSlickText">Месники</p>
                 <p className="slickText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor text update, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat <span>Dimas very good worker</span></p>
             </div>
-            <img src="./image/film.jpg" alt="" className="sliderItemImage"></img>
+            <img src="./image/film.jpg" alt="" className="sliderItemImage" />
         </div>
     )
 }
