4e89c3cf866bc92e5701298272b96b68762a51a2
rzeylik <rzeylik@gmail.com>
2021-10-23 10:31:15 +0300

    text update

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -11,7 +11,7 @@ const Board = () => {
                 <p className="premiereSlickText">Прем’єра</p>
                 <p className="dateSlickTExt">Сьогодні <span>03.09</span></p>
                 <p className="nameSlickText">Месники</p>
-                <p className="slickText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                <p className="slickText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor text update, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
             </div>
             <img src="./image/film.jpg" alt="" className="sliderItemImage"></img>
         </div>
